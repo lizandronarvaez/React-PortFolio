6f58824b890bef5b31c0a769396620b5d9a5e515
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-08-01 12:27:28 +0200

    Modificando estilos y componentes en la pagina Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,23 @@
-import Information from "../../components/Information/Information"
+import { Link } from "react-router-dom"
 import "./Home.css"
+
 const Home = () => {
     return (
-        <main className="contenedor home" id="informacion">
-            <div className="container__informacion" id="container__informacion">
-                <Information />
+        <>
+            <main className="home">
+            <div className="container__informacion" data-aos="zoom-in" data-aos-duration="1500" data-aos-once="true">
+                <h2 className="informacion--titulo">Bienvenido</h2>
+                <div className="informacion__bloque" >
+                    <p><span className="informacion-negrita">Soy un Desarrollador Full-Stack,</span>
+                        si haz llegado hasta aqui, te doy las gracias y te invito a visitar mi sitio web.
+                    </p>
+                    <p>Si quieres conocer mas acerca de mis repositorios, te dejo un enlace mi GitHub.
+                        <Link target="_blank" to={"https://github.com/lizandronarvaez"}>GitHub</Link>
+                    </p>
+                </div>
             </div>
-        </main>
+            </main>
+        </>
     )
 }
 
