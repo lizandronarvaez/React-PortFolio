ef8d032ed8a41971671312e23f132ff34cfdec16
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-08-01 12:30:53 +0200

    Corrigiendo texto en en el componente

--- src/components/Skills/FooterSkills.jsx
+++ src/components/Skills/FooterSkills.jsx
@@ -4,9 +4,8 @@ const FooterSkills = () => {
         <div className="informacion__fullstack" data-aos="fade-right" data-aos-duration="1000" data-aos-once="true">
             <img className="skills" src={FullStack} alt="logo" />
             <p>Si necesitas un desarrollador web con ganas de ampliar sus
-                conocimientos y crecer profesionalmente,
-                abajo te facilito diferentes formas de contactarme, estaré encantado de pertecener a tu equipo y
-                juntos superar retos!. </p>
+                conocimientos y crecer profesionalmente, puedes ponerte en contacto conmigo.
+            </p>
         </div>
     )
 }
