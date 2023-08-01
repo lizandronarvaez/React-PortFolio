d176577b0a8744ef922d45aa40d3639ab419662e
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-08-01 12:09:21 +0200

    Agregando un componente mas especifico

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,41 @@
+import useForm from "../../hooks/useForm";
+import "./Contact.css"
+const Contact = () => {
+    const { form, handleDataFormulario, handleSubmitFormulario } = useForm()
+    const { user_name, user_email, message } = form;
+
+    return (
+        <form className="form" onSubmit={handleSubmitFormulario}>
+            <h2>Formulario de contacto</h2>
+            <hr />
+            <div className="formulario">
+                <div className="formulario_box">
+                    <input type="text"
+                        onChange={handleDataFormulario}
+                        placeholder="Asunto Mensaje"
+                        name="user_name"
+                        value={user_name} />
+                </div>
+                <div className="formulario_box">
+                    <input type="email"
+                        onChange={handleDataFormulario}
+                        placeholder="Introduce un email"
+                        name="user_email"
+                        value={user_email} />
+                </div>
+                <div className="formulario_box">
+                    <textarea
+                        onChange={handleDataFormulario}
+                        placeholder="Introduce un mensaje"
+                        name="message"
+                        value={message}></textarea>
+                </div>
+                <button type="submit">
+                    Enviar
+                </button>
+            </div>
+        </form>
+    )
+}
+
+export default Contact
