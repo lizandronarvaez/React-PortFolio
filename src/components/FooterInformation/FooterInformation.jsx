6fde7caa7aa6dceb2000c13469758ef5506d110d
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-08-01 12:02:19 +0200

    Creando componente Footer

---
+++ src/components/FooterInformation/FooterInformation.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+import Linkedin from "../../assets/linkedin.svg"
+import "./FooterInformation.css"
+
+const FooterInformation = () => {
+    return (
+        <div className="footer">
+            <div className="iconos">
+                <Link
+                    to="https://www.linkedin.com/in/lizandro-narvaez"
+                >
+                    <img src={Linkedin} alt="linkedin" />
+                    <p>Linkedin</p>
+                </Link>
+            </div>
+            <p className="footer__texto">PortFolio realizado por Lizandro Narvaez</p>
+        </div>
+    )
+}
+
+export default FooterInformation
