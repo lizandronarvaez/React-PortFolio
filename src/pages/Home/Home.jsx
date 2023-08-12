891f2835995dd0dbd0d77c0dd4867746822465ab
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-08-12 20:38:16 +0200

    Cambios en la linea

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,17 +5,18 @@ const Home = () => {
     return (
         <>
             <main className="home">
-            <div className="container__informacion" data-aos="zoom-in" data-aos-duration="1500" data-aos-once="true">
-                <h2 className="informacion--titulo">Bienvenido</h2>
-                <div className="informacion__bloque" >
-                    <p><span className="informacion-negrita">Soy un Desarrollador Full-Stack,</span>
-                        si haz llegado hasta aqui, te doy las gracias y te invito a visitar mi sitio web.
-                    </p>
-                    <p>Si quieres conocer mas acerca de mis repositorios, te dejo un enlace mi GitHub.
-                        <Link target="_blank" to={"https://github.com/lizandronarvaez"}>GitHub</Link>
-                    </p>
+                <div className="container__informacion" data-aos="zoom-in" data-aos-duration="1500" data-aos-once="true">
+                    <h2 className="informacion--titulo">Bienvenido</h2>
+                    <div className="informacion__bloque" >
+                        <p>
+                            <span className="informacion-negrita">Soy un Desarrollador Full-Stack,</span>
+                            si haz llegado hasta aqui, te doy las gracias y te invito a visitar mi sitio web.
+                        </p>
+                        <p>Si quieres conocer mas acerca de mis repositorios, te dejo un enlace mi GitHub.
+                            <Link target="_blank" to={"https://github.com/lizandronarvaez"}>GitHub</Link>
+                        </p>
+                    </div>
                 </div>
-            </div>
             </main>
         </>
     )
