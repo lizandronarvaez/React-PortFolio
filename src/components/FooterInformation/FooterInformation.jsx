adc0b3d59b80d91a4041e5d1ac597776419db99e
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-09-12 08:55:04 +0200

    Cambiando etiqueta <p> por <h5>

--- src/components/FooterInformation/FooterInformation.jsx
+++ src/components/FooterInformation/FooterInformation.jsx
@@ -1,21 +1,18 @@
-import { Link } from "react-router-dom"
-import Linkedin from "../../assets/linkedin.svg"
-import "./FooterInformation.css"
+import { Link } from "react-router-dom";
+import Linkedin from "../../assets/linkedin.svg";
+import "./FooterInformation.css";
 
 const FooterInformation = () => {
-    return (
-        <div className="footer">
-            <div className="iconos">
-                <Link
-                    to="https://www.linkedin.com/in/lizandro-narvaez"
-                >
-                    <img src={Linkedin} alt="linkedin" />
-                    <p>Linkedin</p>
-                </Link>
-            </div>
-            <p className="footer__texto">Powered by Lizandro Narvaez</p>
-        </div>
-    )
-}
+  return (
+    <div className="footer">
+      <h5 className="footer__texto">Powered by Lizandro Narvaez</h5>
+      <div className="iconos">
+        <Link to="https://www.linkedin.com/in/lizandro-narvaez">
+          <img src={Linkedin} alt="linkedin" />
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default FooterInformation
+export default FooterInformation;
