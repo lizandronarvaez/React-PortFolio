1f12fd4edf931dcdf1204357d70a06c2a1b2612c
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-11-18 17:11:41 +0100

    Organiznado el codigo, haciendolo mas legible

--- src/interface/Nav/ItemNav/ItemNav.jsx
+++ src/interface/Nav/ItemNav/ItemNav.jsx
@@ -1,21 +1,24 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom"
-import iconNav from "../../../assets/icon-nav.svg"
 import "./ItemNav.css"
+import { iconNav } from "../../../assets"
 
-const ItemNav = ({toggleClass}) => {
+const ItemNav = ({ toggleClass }) => {
 
     return (
         <div className="logotipos">
-            <img className="logotipos__img" src={iconNav} alt="logotipo" />
+            <img
+                className="logotipos__img"
+                src={iconNav}
+                alt="logotipo"
+            />
             <h1 className="logotipos__titulo">
-                <Link className="titulo__enlace"
-                    to="/"
-                >
+                <Link className="titulo__enlace" to="/">
                     Lizandro Narvaez
                 </Link>
             </h1>
-            <div className="hamburger"
+            <div
+                className="hamburger"
                 onClick={toggleClass}
             >
                 <div className="hamburger__item"></div>
--- src/interface/Nav/Nav.jsx
+++ src/interface/Nav/Nav.jsx
@@ -1,22 +1,21 @@
 import { useState } from 'react';
-import ItemNav from './ItemNav/ItemNav'
-import ItemNavEnlaces from './ItemNavEnlaces/ItemNavEnlaces'
-import "./nav.css"
+import ItemNav from './ItemNav/ItemNav';
+import ItemNavEnlaces from './ItemNavEnlaces/ItemNavEnlaces';
+import "./nav.css";
 const Nav = () => {
     const [isActive, setIsActive] = useState(false);
-
-    const toggleClass = () => {
-        if (window.innerWidth <= 768) {
-            setIsActive(!isActive);
-        }
-    }
+    const toggleClass = () => window.innerWidth <= 768 ? setIsActive(!isActive) : null;
     return (
-        <nav className="navegacion" id="barra" data-aos="zoom-in" data-aos-duration="1500" data-aos-once="true">
+        <nav
+            className="navegacion"
+            data-aos="zoom-in"
+            data-aos-duration="1500"
+            data-aos-once="true"
+        >
             <ItemNav toggleClass={toggleClass} />
             <ItemNavEnlaces isActive={isActive} />
         </nav>
     )
-
 }
 
 export default Nav
