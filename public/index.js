5f30e42794ccd46cf0e98b1ad35af4c447c56b53
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-11-18 18:10:14 +0100

    Agragando nuevos datos al archivo barril

--- public/index.js
+++ public/index.js
@@ -1,5 +1,17 @@
 import fullStack from "./img/fullstack-title.png";
 import photoProfile from "./img/foto.png";
+import portalEmpleo from "./img/portalEmpleo.png";
+import rickAndMorty from "./img/rick_morty.png";
+import weatherApp from "./img/weatherApp.png";
+import gifApp from "./img/gifApp.png";
+import portfolio from "./img/portfolio.png";
 
-
-export { fullStack, photoProfile };
+export {
+    fullStack,
+    photoProfile,
+    portalEmpleo,
+    rickAndMorty,
+    weatherApp,
+    gifApp,
+    portfolio
+};
