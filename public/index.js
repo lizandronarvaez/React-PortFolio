442421c935635de17784c7978f013a7ff1436522
Lizandro <lizandrojesus13@hotmail.com>
2024-05-13 19:44:04 +0200

    Feat: Agregando nuevas importaciones al archivo barril

--- public/index.js
+++ public/index.js
@@ -1,10 +1,12 @@
 import fullStack from "./img/fullstack-title.png";
-import photoProfile from "./img/foto.jpg";
+import photoProfile from "./img/image_profile.png";
 import portalEmpleo from "./img/portalEmpleo.png";
 import rickAndMorty from "./img/rick_morty.png";
 import weatherApp from "./img/weatherApp.png";
 import gifApp from "./img/gifApp.png";
 import portfolio from "./img/portfolio.png";
+import protein_shop from "./img/protein_shop.png";
+import crm_frontend from "./img/crm_frontend.png";
 
 export {
     fullStack,
@@ -13,5 +15,7 @@ export {
     rickAndMorty,
     weatherApp,
     gifApp,
-    portfolio
+    portfolio,
+    protein_shop,
+    crm_frontend
 };
