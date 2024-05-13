32ba67a12a3b0cf7d80e35c085dc4a52e60e85c7
Lizandro <lizandrojesus13@hotmail.com>
2024-05-13 19:44:49 +0200

    Feat: Agregando nuevo proyectos desarrollados y en desarrollo

--- src/pages/Projects/projectsList.js
+++ src/pages/Projects/projectsList.js
@@ -1,37 +1,32 @@
-import { gifApp, portalEmpleo, portfolio, rickAndMorty, weatherApp } from "../../../public"
+import { crm_frontend, portalEmpleo, portfolio , protein_shop} from "../../../public"
 
 export const projectsList = [
+    
     {
         "title": "PortFolio",
         "img": portfolio,
         "descripcion": "PortFolio realizado con Reactjs, React-Router, Hooks, SweetAlert y EmailJs para el formulario de Contacto",
         "repositorio": "https://github.com/lizandronarvaez/React-PortFolio",
-    }
-    ,
-    {
-        "title": "WeatherApp",
-        "img": weatherApp,
-        "descripcion": "App Realizada con Reactjs, muestra la temperatura de ciudades españolas",
-        "repositorio": "https://github.com/lizandronarvaez/React-app-weather",
-        "enlaceProyecto": "https://react-app-weather-psi.vercel.app"
     },
     {
-        "title": "GifApp",
-        "img": gifApp,
-        "descripcion": "App de busqueda de Gifs realizada con Reactjs,",
-        "repositorio": "https://github.com/lizandronarvaez/gifs-app-reactjs",
-        "enlaceProyecto": "https://gifs-app-reactjs.vercel.app"
-    }, {
+        "title": "E-Commerce ProteinSHOP",
+        "img": protein_shop,
+        "descripcion": "E-Commerce en desarrollo consumiendo productos desde una api y utilizando ReactJs para el frontend",
+        "repositorio": "https://github.com/lizandronarvaez/App-ProteinShop-React",
+        "enlaceProyecto": "https://app-protein-shop-react.vercel.app/products"
+    },
+    {
+        "title": "Crm Sistema de Gestión",
+        "img": crm_frontend,
+        "descripcion": "Crm de gestión de productos, pedidos y clientes, desarrollado con SpringBoot, Spring Security y utilizando ReactJs para el frontend",
+        "repositorio": "https://github.com/lizandronarvaez/FullStack-Frontend-Reactjs",
+        "enlaceProyecto": "https://crm-frontend-alpha.vercel.app/login"
+    },
+    {
         "title": "DevJobs",
         "img": portalEmpleo,
         "descripcion": "Portal de empleo realizado con NodeJs,ExpressJs, Handlebars, Autenticaciones, Notificaciones de Registro con Nodemailer, ODM Mongoose.",
         "repositorio": "https://github.com/lizandronarvaez/DevJobs",
         "enlaceProyecto": "https://empleo-it-devjobs.onrender.com"
-    }, {
-        "title": "Rick and Morty",
-        "img": rickAndMorty,
-        "descripcion": "SingleApp realizada con ReactJs, contiene un formulario para buscar los personajes.",
-        "repositorio": "https://github.com/lizandronarvaez/Rick-Morty-API",
-        "enlaceProyecto": "https://rick-morty-api-phi.vercel.app"
     }
 ]
